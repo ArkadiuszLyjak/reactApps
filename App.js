e2145bbc83b486b05ebc20023b6bd538114354b8
alyjak <alyjak@gmail.com>
2024-05-31 09:48:39 +0200

    Addes props and states.

--- App.js
+++ App.js
@@ -1,228 +1,91 @@
-const header = <h1 className='className'>Witaj</h1>;
+const header = <h1 className="title">Witaj na mojej stronie!</h1>
 
 const classBig = "big";
 
+const handleClick = () => alert("Zostało kliknięte...")
+
 const main = (
     <div>
-        <h1 className='red'>Pierwszy artykół</h1>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit repellat
-            deserunt, sequi quia voluptatum quis adipisci iure, eius maxime maiores
-            voluptatem ad soluta veritatis tenetur! Odio, facere temporibus. Repellat,
-            ipsa?
+        <h1 onClick={handleClick} className="red">Pierwszy artykół</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia
+            odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
+            Curabitur venenatis neque sit amet felis suscipit, a lacinia odio elementum.
+            Phasellus hendrerit neque a urna aliquet, ut sollicitudin sapien dapibus.
+            Sed consectetur massa a odio tincidunt, non bibendum libero cursus. Nullam
+            vel turpis eu lorem suscipit facilisis. Nam eget sem augue. Suspendisse potenti.
+            Etiam ac bibendum mauris. Pellentesque habitant morbi tristique senectus et
+            netus et malesuada fames ac turpis egestas. Vivamus convallis turpis vel enim
+            blandit, sit amet ultricies nisl ultricies.
         </p>
+
     </div>
-);
+)
 
+//region component_klasowy
+class ShoppingList extends React.Component {
 
-class City {
+    state = {
+        items1: 'ogórki',
+        items2: 'sok',
+        items3: 'coś do picia'
+    }
 
+    render() {
+        return (
+            <>
+                <h1>Lista zakupów</h1>
+                <ul>
+                    <ItemList name={this.state.items1} example={2 + 2}/>
+                    <ItemList name={this.state.items2} example={2 + 2}/>
+                    <ItemList name={this.state.items3} example={2 + 2}/>
+                </ul>
+            </>
+        )
+    }
 }
 
-const Warsaw = new City()
-console.log(Warsaw)
-
-// const fn = (item1, item2)=> {
-// 	console.log(`Podany argument to: ${item1} oraz ${item2}`)
-// }
-//
-// fn("hej", "hek")
-
-const user = [2, 3, 4, 5]
-// const users_joined = user.join(" ")
-// console.log(users_joined)
-
-// const doubled_numbers = user.forEach(num => console.log(`${num}, zdublowane: ${num*2})`);
-// console.log(doubled_numbers)
-// console.log(user.forEach(num => console.log(`${num}, zdublowane: ${num*2})`))
-
-// const numbers = [1, 2, 3, 4, 5]; // Przykładowa tablica liczb
-//
-// numbers.forEach(num => {
-//     console.log(`${num}, zdublowane: ${num * 2}`);
-// });
-//
-// console.log(numbers);
+class ItemList extends React.Component {
+    render() {
+        return (
+            <li>{this.props.name} - {this.props.example}</li>
+        )
+    }
+}
 
 
-// function isString(value) {
-//     return typeof value === "string" || value instanceof String;
-// }
+ReactDOM.render(<ShoppingList/>, document.getElementById('element_shopping_list'))
 
-// function isString(value) {
-// 	return typeof value === "string" || value instanceof String;
-// }
+//endregion
 
-// const users = ["adam", new  String("Bogdan"), "czarek", "darek"];
+//region button
+class App extends React.Component {
 
-// const lowercaseUsers = users.map(user => {
-// 	if (isString(user)) {
-// 		return user.toLowerCase();
-// 	} else {
-// 		// Obsługa przypadku, gdy element nie jest łańcuchem znaków
-// 		return user;
-// 	}
-// });
-//
-// console.log(lowercaseUsers);
-//
-// const new_table = [...users, user]
+    state = {
+        text: ""
+    }
 
-// const big_letter = users.map(user => user[0].toUpperCase())
-// console.log(big_letter)
+    handleClick() {
+        const letterToAdd = 'A';
+        this.setState(() => {
+            return {
+                text: this.state.text + letterToAdd
+                
+            }
+        })
 
-// const users = ["Alice", "Bob", new String("Charlie")]; // Tablica zawierająca stringi i obiekt String
-//
-// // Używamy map do przekształcenia każdego elementu na jego długość
-// const lengths = users.map(
-// 	(user, index, array) => {
-//     console.log(`Index: ${index}, Element: ${user}, Length: ${user.length}, ${array[index]}`);
-//     return user.length;
-// });
-//
-// console.log(lengths); // Wyświetli [5, 3, 7]
+    }
 
 
-// const user_ages = [21, 22, 34, 56, 78];
-//
-// const nums_greater_than_22 = user_ages.filter((num, index, talica) => {
-// 	return num >= 23;
-// });
-//
-// console.log(nums_greater_than_22)
+    render() {
+        return (
+            <>
+                <button onClick={this.handleClick.bind(this)}>Dodaj "A"</button>
+                <h1>{this.state.text}</h1>
+            </>
+        )
+    }
+}
 
+ReactDOM.render(<App/>, document.getElementById("root"))
 
-// const people = [
-//     {"name": "Alice", "age": 27},
-//     {"name": "Bob", "age": 30},
-//     {"name": "Charlie", "age": 12},
-//     {"name": "Diana", "age": 28},
-//     {"name": "Eve", "age": 35},
-//     {"name": "Frank", "age": 40},
-//     {"name": "Grace", "age": 32},
-//     {"name": "Hank", "age": 27},
-//     {"name": "Ivy", "age": 26},
-//     {"name": "Jack", "age": 16}
-// ];
-
-// console.log(people);
-
-// const isUserAdult = people.findIndex(person  => person.age < 18);
-// console.log(isUserAdult)
-
-// const underAgedPeople = people.filter((people, index, array) => {
-//     return people.age < 18;
-// });
-
-// console.log(underAgedPeople)
-
-// const alice = people.find((element, index, array) => {
-//     return element.name === "Alice";
-// });
-
-// console.log(alice)
-// const alice = people.findIndex((element, index, array) => {
-//     return element.name === "Alice";
-// });
-//
-// console.log(alice)
-
-// user_ages.forEach((value, index, array) => {
-// console.log(value+10, index, array)
-// });
-
-// const age_doubled = user_ages.map((value, index, array) => {
-//     console.log(`${value + 1}, ${index}, ${array[index]}`);
-//     return value * 2; // Zwracamy zdublowaną wartość
-// });
-//
-// console.log(age_doubled); // Wyświetli [42, 44, 68, 112, 156]
-
-
-// const new_letter_upper_case = users.map(elem => elem[0].toUpperCase())
-// const new_letter_upper_case = users_joined.map(elem => elem[0].toUpperCase())
-// console.log(new_letter_upper_case)
-
-// console.log(typeof "Dupa")
-// console.log("Dupa" instanceof String)
-// const dupa = new String("Dupa")
-// console.log(dupa instanceof String)
-
-
-// console.log(isString("Dupa")); // true
-// console.log(isString(new String("Dupa"))); // true
-// console.log(isString(42)); // false
-// console.log(isString({})); // false
-
-
-// console.log(typeof users);
-// console.log(typeof new_table);
-// console.log(typeof 42); // "number"
-// console.log(typeof "Hello, world!"); // "string"
-// console.log(typeof true); // "boolean"
-// console.log(typeof undefined); // "undefined"
-// console.log(typeof { name: "Alice" }); // "object"
-// console.log(typeof null); // "object" (jest to specyficzny przypadek w JavaScript)
-// console.log(typeof function() {}); // "function"
-// console.log(typeof Symbol()); // "symbol"
-// console.log(typeof BigInt(123)); // "bigint"
-
-// console.log([] instanceof Array); // true
-// console.log({} instanceof Object); // true
-// console.log(function() {} instanceof Function); // true
-// console.log(new Date() instanceof Date); // true
-
-// console.log(Array.isArray([])); // true
-// console.log(Array.isArray({})); // false
-
-// console.log(Object.prototype.toString.call([])); // "[object Array]"
-// console.log(Object.prototype.toString.call({})); // "[object Object]"
-// console.log(Object.prototype.toString.call(new Date())); // "[object Date]"
-// console.log(Object.prototype.toString.call(/regex/)); // "[object RegExp]"
-// console.log(Object.prototype.toString.call(null)); // "[object Null]"
-// console.log(Object.prototype.toString.call(undefined)); // "[object Undefined]"
-// console.log(Object.prototype.toString.call(123)); // "[object Number]"
-// console.log(Object.prototype.toString.call("abc")); // "[object String]"
-// console.log(Object.prototype.toString.call(true)); // "[object Boolean]"
-// console.log(Object.prototype.toString.call(Symbol())); // "[object Symbol]"
-// console.log(Object.prototype.toString.call(BigInt(123))); // "[object BigInt]"
-
-// function getType(variable) {
-//     if (variable === null) {
-//         return "null";
-//     }
-//     if (Array.isArray(variable)) {
-//         return "array";
-//     }
-//     if (variable instanceof Date) {
-//         return "date";
-//     }
-//     if (variable instanceof RegExp) {
-//         return "regexp";
-//     }
-//     return typeof variable;
-// }
-//
-// console.log(getType(42)); // "number"
-// console.log(getType("Hello, world!")); // "string"
-// console.log(getType(true)); // "boolean"
-// console.log(getType(undefined)); // "undefined"
-// console.log(getType({ name: "Alice" })); // "object"
-// console.log(getType(null)); // "null"
-// console.log(getType([])); // "array"
-// console.log(getType(function() {})); // "function"
-// console.log(getType(new Date())); // "date"
-// console.log(getType(/regex/)); // "regexp"
-// console.log(getType(Symbol())); // "symbol"
-// console.log(getType(BigInt(123))); // "bigint"
-
-
-const footer = (
-    <footer>
-        <p className={classBig}>Footer na razie niezrobiony...</p>
-    </footer>
-);
-
-const app = [header, main, footer];
-
-ReactDOM.render(app, document.getElementById("root"));
+//endregion
